9350ea9fb943815e29f709c3c0a9d7ce0d47e2eb
Nicolas Spath <nicolas.spath@web.de>
2026-03-26 19:44:06 +0900

    Organize UI Scripts. Add Start Menu to select endless game or levels

---
+++ Scripts/ui/Buttons/main_ui/StartButton.gd
@@ -0,0 +1,11 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    pressed.connect(Callable(self, "_on_pressed"))
+
+
+func _on_pressed():
+    get_parent().find_child("BackButtonStart").visible = true
+    visible = false
